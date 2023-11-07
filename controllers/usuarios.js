b583687422d71eb90d218f5bf1322cb4159ef611
GeovannyTS <yowtecum@gmail.com>
2023-11-07 16:54:31 -0600

    Separate functions from requests
    Add controless & users File. Cotains functions http

---
+++ controllers/usuarios.js
@@ -0,0 +1,47 @@
+// Se importa porque no sabe que es res por ello se coloca res = response. Aunque sea redundante
+const { response } = require('express')
+
+// Crear funciones y exportarlas al archivo de routes/usuarios
+
+const usuariosGet = (req, res = response) => {
+    res.json({
+        msg: 'Get API response get - controllers'
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+    res.json({
+        msg: 'Get API response put - controllers'
+    });
+}
+
+const usuariosPost = (req, res = response) => {
+    res.json({
+        msg: 'Get API response post - controllers'
+    });
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg: 'Get API response delete - controllers'
+    });
+}
+
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        msg: 'Get API response patch - controllers'
+    });
+}
+
+
+
+
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete,
+    usuariosPatch,
+}
