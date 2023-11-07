a94416424780cdf66b20133a8ecf9845ca73b3dd
GeovannyTS <yowtecum@gmail.com>
2023-11-07 17:19:27 -0600

    Params & Query from GET

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,28 +1,41 @@
 // Se importa porque no sabe que es res por ello se coloca res = response. Aunque sea redundante
-const { response } = require('express')
+const { response, request } = require('express')
 
 // Crear funciones y exportarlas al archivo de routes/usuarios
 
-const usuariosGet = (req, res = response) => {
+const usuariosGet = (req = request, res = response) => {
+
+    // const query = req.query;
+    const {q, nombre, apiKey} = req.query;
+
     res.json({
-        msg: 'Get API response get - controllers'
+        msg: 'Get API response get - controllers',
+        q, 
+        nombre,
+        apiKey
     });
 }
 
 const usuariosPut = (req, res = response) => {
+
+    // En la ruta de indico que es ID y se obtoene de la siguiente forma
+    const {id} = req.params;
+
     res.json({
-        msg: 'Get API response put - controllers'
+        msg: 'Get API response put - controllers',
+        id
     });
 }
 
 const usuariosPost = (req, res = response) => {
 
     // Aca extraemos la data que se envia
-    const body = req.body;
+    const {nombre, edad} = req.body;
 
     res.json({
         msg: 'Get API response post - controllers',
-        body
+        nombre, 
+        edad
     });
 }
 
