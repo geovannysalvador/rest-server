e1fb6271e662a0993082c996b57ac113e1b8e3fe
GeovannyTS <yowtecum@gmail.com>
2023-11-21 17:23:40 -0600

    Using rols in DB validators

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -4,6 +4,7 @@ const { Router } = require('express');
 const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } = require('../controllers/usuarios');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
+const Role = require('../models/role');
 
 const router = Router();
 
@@ -17,7 +18,13 @@ router.post('/', [
     check('nombre', 'El nombre no es valido').not().isEmpty(),
     check('correo', 'El correo no es valido').isEmail(),
     check('password', 'El password debe tener 6 letras').isLength({min: 6}),
-    check('rol', 'No es un rol valido').isIn(['ADMIN', 'USER']),
+    // check('rol', 'No es un rol valido').isIn(['ADMIN', 'USER']),
+    check('rol').custom( async (rol = "") => {
+        const existeRol = await Role.findOne({rol});
+        if (!existeRol){
+            throw new Error(`El rol ${rol} no esta permitido`);
+        }
+    }),
     validarCampos
 ], usuariosPost);
 
